e6f22950a3baee2cb25f942761f32be0b5024904
joywin2003 <21a20.joywin@sjec.ac.in>
2023-09-24 12:14:37 +0530

    new computer canva

--- src/components/Cube.jsx
+++ src/components/Cube.jsx
@@ -2,7 +2,6 @@ import React, { useRef } from "react";
 import { PerspectiveCamera, RenderTexture, Text } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 
-
 const Cube = () => {
   const textRef = useRef();
   useFrame(
